abf005c122f4ea3c4648af48ff6562b0371cf042
GKHalbert <53063801+GKHalbert@users.noreply.github.com>
2020-08-05 11:27:44 -0400

    Add files via upload

--- ProjectAmber/config/helpers.js
+++ ProjectAmber/config/helpers.js
@@ -1,1 +1,15 @@
+const Mysqli = require('mysqli');
 
+let conn = new Mysqli({
+    host: 'localhost', 
+    post: 3306,
+    user: 'Amber',
+    passwd: '114514', 
+    db: 'store_db' 
+})
+
+let db = conn.emit(false, '')
+
+module.exports = {
+    database: db
+}

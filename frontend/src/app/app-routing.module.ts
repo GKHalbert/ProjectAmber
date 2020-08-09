f324c4227296180a1785563df274031f2a1dff84
GKHalbert <jingwei.shang@mail.utoronto.ca>
2020-08-08 21:00:13 -0400

    Add routes.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component'
+import {ProductComponent} from './components/product/product.component'
+import {CartComponent} from './components/cart/cart.component'
+import {CheckoutComponent} from './components/checkout/checkout.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
